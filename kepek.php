4b27596761fab9d9fc0d7f8d14a443eab40abb60
bgeri0715 <57726812+bgeri0715@users.noreply.github.com>
2019-11-13 22:57:26 +0100

    Add files via upload

--- kepek.php
+++ kepek.php
@@ -1,13 +1,35 @@
-<!DOCTYPE html>
+<?php
+    // Alkalmazás logika:
+    include('config.inc.php');
+
+    // adatok összegyűjtése:
+    $kepek = array();
+    $olvaso = opendir($MAPPA);
+    while (($fajl = readdir($olvaso)) !== false) {
+        if (is_file($MAPPA.$fajl)) {
+            $vege = strtolower(substr($fajl, strlen($fajl)-4));
+            if (in_array($vege, $TIPUSOK)) {
+                $kepek[$fajl] = filemtime($MAPPA.$fajl);
+            }
+        }
+    }
+    closedir($olvaso);
+
+    // Megjelenítés logika:
+?><!DOCTYPE html>
 <!--[if IE 8 ]><html class="no-js oldie ie8" lang="en"> <![endif]-->
 <!--[if IE 9 ]><html class="no-js oldie ie9" lang="en"> <![endif]-->
 <!--[if (gte IE 9)|!(IE)]><!--><html class="no-js" lang="en"> <!--<![endif]-->
 <head>
-
+  <style type="text/css">
+      div#galeria {margin: 0 auto; width: 620px;}
+      div.kep { display: inline-block; }
+      div.kep img { width: 200px; }
+  </style>
    <!--- basic page needs
    ================================================== -->
    <meta charset="utf-8">
-	<title>Kezdőoldal - Abstract</title>
+	<title>Heti beosztás - Abstract</title>
 	<meta name="description" content="">
 	<meta name="author" content="">
 
@@ -26,6 +48,7 @@
    ================================================== -->
 	<script src="js/modernizr.js"></script>
 	<script src="js/pace.min.js"></script>
+  <script src="js/kapcsolat.js"></script>
 
    <!-- favicons
 	================================================== -->
@@ -48,25 +71,26 @@
 	         <a href="index.html">Author</a>
 	      </div>
 
-	   	<nav id="main-nav-wrap">
-				<ul class="main-navigation sf-menu">
-					<li class="current"><a href="index.html" title="">Kezdőoldal</a></li>
-						<li><a href="beosztas.html" title="">Heti beosztás</a>
-            <li><a href="klub.html" title="">A klubról</a>
-              <li><a href="klub.html" title="">Képek</a>
-                <li><a href="klub.html" title="">Képek feltöltése</a>
-				</ul>
-			</nav> <!-- end main-nav-wrap -->
+        <nav id="main-nav-wrap">
+  				<ul class="main-navigation sf-menu">
+  					<li><a href="index.html" title="">Kezdőoldal</a></li>
+  						<li><a href="beosztas.html" title="">Heti beosztás</a>
+              <li><a href="klub.html" title="">A klubról</a>
+                <li class="current"><a href="kepek.php" title="">Képek</a>
+                  <li><a href="kepek_feltolt.php" title="">Képek feltöltése</a>
+  				</ul>
+  			</nav> <!-- end main-nav-wrap -->
 
 			<div class="search-wrap">
 
-				<form role="search" method="GET" class="search-form" action="https://www.google.hu/search?hl=en-GB&source=hp&q=">
+        <form role="search" method="GET" class="search-form" action="https://www.google.hu/search?hl=en-GB&source=hp&q=">
 					<label>
 						<span class="hide-content">Keresés:</span>
 						<input type="search" class="search-field" placeholder="Keresés..." value="" name="q" id="s" title="Search for:" autocomplete="off">
 					</label>
 					<input type="submit" class="search-submit" value="Search">
 				</form>
+
 				<a href="#" id="close-search" class="close-btn">Close</a>
 
 			</div> <!-- end search wrap -->
@@ -89,23 +113,31 @@
 
    			<article class="format-standard">
 
-          Hírek 2019<br>
-  Tagdíj!<p>
-  Kedves Versenyzők, Kedves Szülők!<p>
-
-  Kérném szépen az elmúlt időszakban elmaradt tagdíjbefizetést pótolni.<p>
-
-  A tagdíj továbbra is 6000.-Ft./hó.<p>
-
-  Bankszámlaszám: 69300224-10006777<p>
-
-  20191123 Szolnok (szakaszos) Aquatlon verseny (mindenki)<p>
+          <div id="galeria">
+          <h1>Galéria</h1>
+          <?php
+          arsort($kepek);
+          foreach($kepek as $fajl => $datum)
+          {
+          ?>
+              <div class="kep">
+                  <a href="<?php echo $MAPPA.$fajl ?>">
+                      <img src="<?php echo $MAPPA.$fajl ?>">
+                  </a>
+                  <p>Név:  <?php echo $fajl; ?></p>
+                  <p>Dátum:  <?php echo date($DATUMFORMA, $datum); ?></p>
+              </div>
+          <?php
+          }
+          ?>
+          </div>
 
 				</article>
 
 
 			</div> <!-- end col-twelve -->
    	</div> <!-- end row -->
+
 
    </section> <!-- end content -->
 
@@ -118,16 +150,16 @@
 
    		<div class="row">
 
-	      	<div class="col-two tab-1-3 mob-1-2 site-links">
 
-            <h4>Linkek</h4>
+	      	<div class="col-two tab-1-3 mob-1-2 social-links">
 
-            <ul>
-            <li><a href="https://www.facebook.com/home.php#!/pages/MÚTK-Martfűi-Úszó-és-Triatlon-Klub/106474766101055">Facebook</a></li>
+	      		<h4>Közösségi oldal</h4>
 
-          </ul>
+	      		<ul>
+						<li><a href="https://www.facebook.com/home.php#!/pages/MÚTK-Martfűi-Úszó-és-Triatlon-Klub/106474766101055">Facebook</a></li>
+					</ul>
 
-	      	</div> <!-- end site-links -->
+	      	</div> <!-- end social links -->
 
 	      </div> <!-- end row -->
 
--- kepek_feltolt.php
+++ kepek_feltolt.php
@@ -1,4 +1,32 @@
-<!DOCTYPE html>
+<?php
+    // Alkalmazás logika:
+    include('config.inc.php');
+    $uzenet = array();
+
+    // Űrlap ellenőrzés:
+    if (isset($_POST['kuld'])) {
+        //print_r($_FILES);
+        foreach($_FILES as $fajl) {
+            if ($fajl['error'] == 4);   // Nem töltött fel fájlt
+            elseif (!in_array($fajl['type'], $MEDIATIPUSOK))
+                $uzenet[] = " Nem megfelelő típus: " . $fajl['name'];
+            elseif ($fajl['error'] == 1   // A fájl túllépi a php.ini -ben megadott maximális méretet
+                        or $fajl['error'] == 2   // A fájl túllépi a HTML űrlapban megadott maximális méretet
+                        or $fajl['size'] > $MAXMERET)
+                $uzenet[] = " Túl nagy állomány: " . $fajl['name'];
+            else {
+                $vegsohely = $MAPPA.strtolower($fajl['name']);
+                if (file_exists($vegsohely))
+                    $uzenet[] = " Már létezik: " . $fajl['name'];
+                else {
+                    move_uploaded_file($fajl['tmp_name'], $vegsohely);
+                    $uzenet[] = ' Ok: ' . $fajl['name'];
+                }
+            }
+        }
+    }
+    // Megjelenítés logika:
+?><!DOCTYPE html>
 <!--[if IE 8 ]><html class="no-js oldie ie8" lang="en"> <![endif]-->
 <!--[if IE 9 ]><html class="no-js oldie ie9" lang="en"> <![endif]-->
 <!--[if (gte IE 9)|!(IE)]><!--><html class="no-js" lang="en"> <!--<![endif]-->
@@ -7,7 +35,7 @@
    <!--- basic page needs
    ================================================== -->
    <meta charset="utf-8">
-	<title>Kezdőoldal - Abstract</title>
+	<title>Heti beosztás - Abstract</title>
 	<meta name="description" content="">
 	<meta name="author" content="">
 
@@ -26,6 +54,7 @@
    ================================================== -->
 	<script src="js/modernizr.js"></script>
 	<script src="js/pace.min.js"></script>
+  <script src="js/kapcsolat.js"></script>
 
    <!-- favicons
 	================================================== -->
@@ -48,25 +77,26 @@
 	         <a href="index.html">Author</a>
 	      </div>
 
-	   	<nav id="main-nav-wrap">
-				<ul class="main-navigation sf-menu">
-					<li class="current"><a href="index.html" title="">Kezdőoldal</a></li>
-						<li><a href="beosztas.html" title="">Heti beosztás</a>
-            <li><a href="klub.html" title="">A klubról</a>
-              <li><a href="klub.html" title="">Képek</a>
-                <li><a href="klub.html" title="">Képek feltöltése</a>
-				</ul>
-			</nav> <!-- end main-nav-wrap -->
+        <nav id="main-nav-wrap">
+  				<ul class="main-navigation sf-menu">
+  					<li><a href="index.html" title="">Kezdőoldal</a></li>
+  						<li><a href="beosztas.html" title="">Heti beosztás</a>
+              <li><a href="klub.html" title="">A klubról</a>
+                <li><a href="kepek.php" title="">Képek</a>
+                  <li class="current"><a href="kepek_feltolt.php" title="">Képek feltöltése</a>
+  				</ul>
+  			</nav> <!-- end main-nav-wrap -->
 
 			<div class="search-wrap">
 
-				<form role="search" method="GET" class="search-form" action="https://www.google.hu/search?hl=en-GB&source=hp&q=">
+        <form role="search" method="GET" class="search-form" action="https://www.google.hu/search?hl=en-GB&source=hp&q=">
 					<label>
 						<span class="hide-content">Keresés:</span>
 						<input type="search" class="search-field" placeholder="Keresés..." value="" name="q" id="s" title="Search for:" autocomplete="off">
 					</label>
 					<input type="submit" class="search-submit" value="Search">
 				</form>
+
 				<a href="#" id="close-search" class="close-btn">Close</a>
 
 			</div> <!-- end search wrap -->
@@ -89,23 +119,35 @@
 
    			<article class="format-standard">
 
-          Hírek 2019<br>
-  Tagdíj!<p>
-  Kedves Versenyzők, Kedves Szülők!<p>
-
-  Kérném szépen az elmúlt időszakban elmaradt tagdíjbefizetést pótolni.<p>
-
-  A tagdíj továbbra is 6000.-Ft./hó.<p>
-
-  Bankszámlaszám: 69300224-10006777<p>
-
-  20191123 Szolnok (szakaszos) Aquatlon verseny (mindenki)<p>
-
+          <h1>Feltöltés a galériába:</h1>
+      <?php
+          if (!empty($uzenet))
+          {
+              echo '<ul>';
+              foreach($uzenet as $u)
+                  echo "<li>$u</li>";
+              echo '</ul>';
+          }
+      ?>
+          <form action="kepek_feltolt.php" method="post"
+                      enctype="multipart/form-data">
+              <label>Első:
+                  <input type="file" name="elso" required>
+              </label>
+              <label>Második:
+                  <input type="file" name="masodik">
+              </label>
+              <label>Harmadik:
+                  <input type="file" name="harmadik">
+              </label>
+              <input type="submit" name="kuld">
+            </form>
 				</article>
 
 
 			</div> <!-- end col-twelve -->
    	</div> <!-- end row -->
+
 
    </section> <!-- end content -->
 
@@ -118,16 +160,16 @@
 
    		<div class="row">
 
-	      	<div class="col-two tab-1-3 mob-1-2 site-links">
 
-            <h4>Linkek</h4>
+	      	<div class="col-two tab-1-3 mob-1-2 social-links">
 
-            <ul>
-            <li><a href="https://www.facebook.com/home.php#!/pages/MÚTK-Martfűi-Úszó-és-Triatlon-Klub/106474766101055">Facebook</a></li>
+	      		<h4>Közösségi oldal</h4>
 
-          </ul>
+	      		<ul>
+						<li><a href="https://www.facebook.com/home.php#!/pages/MÚTK-Martfűi-Úszó-és-Triatlon-Klub/106474766101055">Facebook</a></li>
+					</ul>
 
-	      	</div> <!-- end site-links -->
+	      	</div> <!-- end social links -->
 
 	      </div> <!-- end row -->
 
